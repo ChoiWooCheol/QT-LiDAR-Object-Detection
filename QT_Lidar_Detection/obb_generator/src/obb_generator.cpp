ed2cfc6ccfc0c5abad7605957e67ac2ea009b9a2
ChoiWooCheol <smartchoi09@naver.com>
2020-02-12 20:53:13 +0900

    add autoware mode, use vectormap mode
    add autoware mode, use vectormap mode

--- QT_Lidar_Detection/obb_generator/src/obb_generator.cpp
+++ QT_Lidar_Detection/obb_generator/src/obb_generator.cpp
@@ -15,10 +15,16 @@
 
 using namespace std::chrono_literals;
 
-#define USE_VECTORMAP 0
+#include <autoware_msgs/DetectedObjectArray.h>
+#include <autoware_msgs/DetectedObject.h>
 
+/* set modes */
+#define USE_VECTORMAP 0
+#define USE_AUTOWARE 0
 #define MODE 0 // 1 is AABB mode
-                // 0 is OBB mode
+               // 0 is OBB mode
+/* --------- */
+    
 #define MAX_CLUSTER_SIZE 100
 
 class ObbGenerator{
@@ -32,13 +38,36 @@ public:
             throw std::runtime_error("set obb_boxes_topic_name");
         if(!private_nh.getParam("maximum_cluster_size", maximum_cluster_size))
             throw std::runtime_error("set maximum_cluster_size");
-
+        
         obb_sub = nh.subscribe(sub_name.c_str(), 1, &ObbGenerator::obb_callback, this);
         obbArr_pub = nh.advertise<jsk_recognition_msgs::BoundingBoxArray>(pub_name.c_str(), 1);
+        autoware_detect_pub = nh.advertise<autoware_msgs::DetectedObjectArray>("/detection/lidar_detector/objects", 1);
     }
     
     ~ObbGenerator(){
 
+    }
+
+    void publishDetectedObjects(const jsk_recognition_msgs::BoundingBoxArray& in_objects)
+    {
+        autoware_msgs::DetectedObjectArray detected_objects;
+        detected_objects.header = in_objects.header;
+
+        for (auto& object : in_objects.boxes)
+        {
+            autoware_msgs::DetectedObject detected_object;
+            detected_object.header = in_objects.header;
+            detected_object.label = "unknown";
+            detected_object.score = 1.;
+            detected_object.space_frame = in_objects.header.frame_id;
+            detected_object.pose = object.pose;
+            detected_object.dimensions = object.dimensions;
+            // detected_object.pointcloud = in_clusters.clusters[i].cloud;
+            // detected_object.convex_hull = in_clusters.clusters[i].convex_hull;
+            detected_object.valid = true;
+            detected_objects.objects.push_back(detected_object);
+        }
+        autoware_detect_pub.publish(detected_objects);
     }
 
 #if USE_VECTORMAP
@@ -91,14 +120,21 @@ public:
             ++z_idx;
             obb_arr.boxes.emplace_back(obb);
         }
+
+#if USE_AUTOWARE
+        publishDetectedObjects(obb_arr);
+#endif
         obbArr_pub.publish(obb_arr);
     }
 
 private:
     ros::NodeHandle nh;
     ros::NodeHandle private_nh;
+
     ros::Subscriber obb_sub;
+
     ros::Publisher obbArr_pub;
+    ros::Publisher autoware_detect_pub;
     std::string pub_name, sub_name;
     pcl::MomentOfInertiaEstimation<pcl::PointXYZ> feature_extractor;
     double maximum_cluster_size;
